332759dcf9c2bb8042c9c6be3bd9aebd1493dc41
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2025-04-29 00:03:18 +0300

    feat: add function to replace - into ---

---
+++ src/lint.py
@@ -0,0 +1,26 @@
+import re
+
+def replace_minuses(latex_content):
+    math_patterns = [
+        r"\$.*?\$",
+        r"\\\[.*?\\\]",
+        r"\\\(.*?\\\)",
+    ]
+    math_pat = "|".join(math_patterns)
+
+    math_content = []
+    def replace_math(match):
+        math_content.append(match.group(0))
+        return f"{{math_block_{len(math_content)-1}}}"
+    
+    latex_content = re.sub(math_pat, replace_math, latex_content)
+
+    latex_content = re.sub(r"-", "---", latex_content)
+
+    def restore_math(match):
+        index = int(match.group(1))
+        return math_content[index]
+    
+    latex_content = re.sub(r"\{math_block_(\d+)\}", restore_math, latex_content)
+
+    return latex_content
