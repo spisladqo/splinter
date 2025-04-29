920f8722da8b53ffbb4c80c12036aa355603ac4a
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2025-04-29 13:16:01 +0300

    format: all code

--- popup/popup.js
+++ popup/popup.js
@@ -1,39 +1,39 @@
 function lintCommand(tabs) {
     browser.tabs.sendMessage(tabs[0].id, {
-      command: "splint"
+        command: "splint"
     });
-  }
-  
-  function reportError(error) {
+}
+
+function reportError(error) {
     console.error(`Could not run lint: ${error}`);
-  }
-  
-  function listenForClicks() {
+}
+
+function listenForClicks() {
     document.addEventListener("click", (e) => {
-      if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
-        return;
-      }
-  
-      browser.tabs.query({ active: true, currentWindow: true })
-        .then(lintCommand)
-        .catch(reportError);
+        if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
+            return;
+        }
+
+        browser.tabs.query({ active: true, currentWindow: true })
+            .then(lintCommand)
+            .catch(reportError);
     });
-  }
-  
-  function reportExecuteScriptError(error) {
+}
+
+function reportExecuteScriptError(error) {
     const popupContent = document.querySelector("#popup-content");
     const errorContent = document.querySelector("#error-content");
-  
+
     if (popupContent) {
-      popupContent.classList.add("hidden");
+        popupContent.classList.add("hidden");
     }
     if (errorContent) {
-      errorContent.classList.remove("hidden");
+        errorContent.classList.remove("hidden");
     }
-  
+
     console.error(`Failed to execute content script: ${error.message}`);
-  }
+}
 
 browser.tabs.executeScript({ file: "/content_scripts/splint.js" })
-  .then(listenForClicks)
-  .catch(reportExecuteScriptError);
+    .then(listenForClicks)
+    .catch(reportExecuteScriptError);
